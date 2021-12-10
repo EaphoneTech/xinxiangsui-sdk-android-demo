c8384d889e422edc4a73e3516346cc2632c4adf7
helinhua <616697293@qq.com>
2021-12-10 17:37:14 +0800

    更新文档

--- app/src/main/java/com/eaphone/sdktest/MyApplication.kt
+++ app/src/main/java/com/eaphone/sdktest/MyApplication.kt
@@ -1,7 +1,6 @@
 package com.eaphone.sdktest
 
 import android.app.Application
-import com.blankj.utilcode.util.LogUtils
 import com.eaphone.lib_sdk.listener.InitResultListener
 import com.eaphone.lib_sdk.sdk.EaphoneInterface
 
@@ -10,17 +9,16 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        EaphoneInterface.init(this, "61b1e60f3e2b3d5a4b457425", "649a613a884c47dda2b530aeadb95c6f", object:
+        EaphoneInterface.init(this, "app_id", "app_secret", object:
             InitResultListener {
             override fun onSucceed() {
-                LogUtils.e("------onSucceed()-----")
+
             }
 
             override fun onError(result: String?) {
-                LogUtils.e("----onError-------"+result)
+
             }
         })
     }
-
 
 }
